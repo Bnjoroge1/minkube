15c25cb31a4bfc249f986bb4d0b9f3108712e1f3
Bill Njoroge <williamsriunge@gmail.com>
2024-08-11 16:43:21 -0400

    adding start and stop worker methods

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 
-	"minkube/node"
-	"minkube/worker"
 	"minkube/manager"
+	"minkube/node"
 	"minkube/task"
+	"minkube/worker"
 
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
+	"github.com/docker/docker/client"
 )
 
 func main() {
@@ -34,20 +36,24 @@ func main() {
 
 	//create workers
 	w := worker.Worker {
+		Name: "worker1",
 		Queue: *queue.New(),
-		Db: make(map[uuid.UUID]task.Task),
+		TaskIds: make(map[uuid.UUID]*task.Task),
 	}
 	fmt.Println("worker: %v \n", w)
 	w.CollectStats()
-	w.RunTask()
-	w.StopTask()
+	w.StartTask(t)
+	//w.RunTask()
+	w.StopTask(t)
 
 	m := manager.Manager{
 		Pending: *queue.New(),
-		TaskDb: make(map[string][]task.Task),
+		TaskDb: make(map[uuid.UUID]task.Task),
 		EventDb: make(map[string][]task.TaskEvent),
 		Workers: []string{w.Name},
-	}
+		WorkersTaskMap: make(map[string][]uuid.UUID),
+		TaskWorkerMap: make(map[uuid.UUID]string),
+	 }
 	fmt.Printf("manager: %v \n", m)
 	m.SelectWorker()
 	m.UpdateTasks()
@@ -62,4 +68,49 @@ func main() {
 		Role: "worker",
 	}
 	fmt.Printf("node: %v \n", n)
+
+	fmt.Printf("Creating a test container \n")
+	dockerTask, createResult := createContainer()
+	if createResult.Error != nil {
+		fmt.Printf("%v", createResult.Error)
+		os.Exit(1)
+	}
+	fmt.Printf("Created container with ID: %s \n", createResult.ContainerId)
+	time.Sleep(time.Second * 5)
+	fmt.Printf("Stopping container %s \n", createResult.ContainerId)
+	_ = stopContainer(dockerTask)
+}
+
+func createContainer() ( *task.Docker, *task.DockerResult) {
+
+		c := task.Config {
+			Name: "test-container-{4}",
+			Image: "postgres:13",
+			Env:  []string{
+				"POSTGRES_USER=minkube",
+				"POSTGRES_PASSWORD=secret",
+			},
+		}
+		dc, _ := client.NewClientWithOpts(client.FromEnv)
+		d := task.Docker {
+			Client: dc,
+			Config: c,
+		}
+		result := d.Run()
+		if result.Error != nil {
+			fmt.Println("%v \n", result.Error)
+			return nil, nil
+		}
+		fmt.Printf("Container %s is running with config %v \n", result.ContainerId)
+		return &d, &result
+}
+
+func stopContainer(d *task.Docker) *task.DockerResult {
+	result := d.Stop(d.ContainerId)
+	if result.Error != nil {
+		fmt.Printf("%v \n", result.Error)
+		return nil
+	}
+	fmt.Printf("Container %s has been stopped and removed \n", result.ContainerId)
+	return &result
 }
--- worker/worker.go
+++ worker/worker.go
@@ -4,11 +4,14 @@ import (
     "github.com/golang-collections/collections/queue"
     "minkube/task"
     "fmt"
+    "time"
+    "log"
+    
 )
-type Worker struct {
+type Worker struct  {
         Name      string
         Queue     queue.Queue
-        Db        map[uuid.UUID]task.Task //stores tasks, using UUIDs as keys and task.Task objects as values
+        TaskIds   map[uuid.UUID]*task.Task   //stores the task ids which can be referenced in the manager by the ID
         TaskCount int
 }
 
@@ -19,10 +22,41 @@ func (w *Worker) CollectStats() {
 func (w *Worker) RunTask() {
 	fmt.Println("Start or stop task")
 }
-func (w *Worker) StartTask() {
-	fmt.Println("I will start a task")
- }
- func (w *Worker) StopTask() {
-	fmt.Println("I will stop a task")
+func (w *Worker) StartTask(t task.Task) task.DockerResult{
+    //starts a given task.
+
+    //set up necessary fields
+    t.StartTime = time.Now().UTC()
+    config := task.NewConfig(&t)
+    d := task.NewDocker(config)
+
+    //run the task
+    result := d.Run()
+    if result.Error != nil {
+        log.Printf("Error starting task %v:%v \n", t.ID, result)
+        t.State = task.Failed
+        w.TaskIds[t.ID] = &t
+        return result
+    }
+    t.State = task.Running
+    t.ContainerID = result.ContainerId
+    w.TaskIds[t.ID] = &t
+    return result
+
+}
+ func (w *Worker) StopTask(t task.Task) task.DockerResult {
+    //receives a task and stops it
+    config := task.NewConfig(&t)
+    d := task.NewDocker(config)
+
+    result := d.Stop(t.ContainerID)
+    if result.Error != nil {
+        log.Printf("Error occurred in worker stopping container %v: %v \n", t.ContainerID, result)
+    }
+    t.EndTime = time.Now().UTC()
+    t.State = task.Completed
+    w.TaskIds[t.ID] = &t 
+    log.Printf("Stopped and removed container %v for task %v \n", t.ContainerID, result)
+    return result 
  }
  
